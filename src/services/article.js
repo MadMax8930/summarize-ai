1f028aed9a5c4b31ccfc6741d7c9deac2ad1f967
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-22 02:43:20 +0200

    articleApi

--- src/services/article.js
+++ src/services/article.js
@@ -3,11 +3,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const articleApi = createApi({
    reducerPath: 'articleApi',
    baseQuery: fetchBaseQuery({
-      baseUrl: ''
+      baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com',
+      prepareHeaders: (headers) => {
+         headers.set('X-RapidAPI-Key', import.meta.env.VITE_RAPID_API_ARTICLE_KEY);
+         headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+         return headers;
+      }
    }),
    endpoints: (builder) => ({
       getSummary: builder.query({
-         query: (params) => `test`
+         query: (params) => 
+         `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=${params.pLength}`
       })
    })
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
+// Fire the hook on demand (LAZY) : NOT AT THE START 
