4c11d59fb5c836a03514204b9bcbf72dfc798e44
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-22 14:23:46 +0200

    import

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,13 @@ import loader from './loader.svg'
 import copy from './copy.svg'
 import logo from './logo.svg'
 import tick from './tick.svg'
+import paragraph from './paragraph.svg'
 
 export {
     linkIcon,
     loader,
     copy,
     logo,
-    tick
+    tick,
+    paragraph
 }
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,22 +1,32 @@
 import { useState, useEffect } from 'react';
-import { copy, linkIcon, loader, tick } from '../assets';
+import { copy, linkIcon, loader, tick, paragraph } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Summary = () => {
-   const [article, setArticle] = useState({
-      url: '',
-      summary: ''
-   });
+   const [article, setArticle] = useState({ url: '', len: '', summary: '' });
+   const [getSummary, { data, isLoading }] = useLazyGetSummaryQuery();
+   const [allArticles, setAllArticles] = useState([]);
 
    const handleSubmit = async (e) => {
-      // Fetch Summary (API request to an AI GPT summarizer)
-      alert('Submitted')
+      e.preventDefault();  // Browser won't reload application
+      // API request
+      const { data } = await getSummary({ articleUrl: article.url, pLength: article.len });
+
+      if (data?.summary) {
+         const newArticle = { ...article, summary: data.summary };
+         const updatedAllArticles = [newArticle, ...allArticles];
+
+         setArticle(newArticle);
+         setAllArticles(updatedAllArticles);
+         console.log(newArticle);
+      }
    };
 
   return (
-    <section className='mt-16 bg-red-100 w-full max-w-xl'>
+    <section className='w-full max-w-xl mt-16 '>
       {/* Search */}
       <div className='flex flex-col w-full gap-2'>
-         <form className='relative flex justify-center items-center'
+         <form className='relative flex justify-center items-center gap-1'
                onSubmit={handleSubmit}
          >
             <img src={linkIcon} alt='link icon' 
@@ -27,6 +37,14 @@ const Summary = () => {
                    onChange={(e) => setArticle({...article, url: e.target.value})}
                    required
                    className='url_input peer'
+            />
+            <img src={paragraph} alt='paragraph icon' 
+                 className='absolute right-20 my-2 mr-4 w-5'/>
+            <input type='number'
+                   placeholder='XX'
+                   value={article.len}
+                   onChange={(e) => setArticle({...article, len: e.target.value})}
+                   className='num_input peer-focus:border-gray-700 peer-focus:text-gray-700' 
             />
             <button type='submit' 
                     className='submit_btn
